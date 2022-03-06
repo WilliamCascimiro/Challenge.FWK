33677c033b2b085534e95bb702af821071dcf041
William Cascimiro da Silva Alves <william.alves@trescon.com.br>
2022-03-06 14:45:33 -0300

    Garantindo que o número informado seja inteiro e maior do que 0.

---
+++ Challenge.App/CalculationService.cs
@@ -0,0 +1,16 @@
+﻿namespace Desafio;
+public class CalculationService
+{
+    public void Start()
+    {
+        int informedNumber;
+        while (true)
+        {
+            Console.WriteLine("Informe um número:");
+            if (int.TryParse(Console.ReadLine(), out informedNumber) && informedNumber > 0)
+                break;
+            Console.WriteLine("Por favor, informe um número inteiro positivo");
+        }
+    }    
+}
+
