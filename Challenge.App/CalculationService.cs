859aa5c499eb4c67f9524493061ea8ddb6d1e53b
William Cascimiro da Silva Alves <william.alves@trescon.com.br>
2022-03-06 15:17:40 -0300

    Implementação dos métodos.

--- Challenge.App/CalculationService.cs
+++ Challenge.App/CalculationService.cs
@@ -11,6 +11,35 @@ public class CalculationService
                 break;
             Console.WriteLine("Por favor, informe um número inteiro positivo");
         }
-    }    
+    }
+
+    #region Methods
+
+    public IEnumerable<int> GetDivisors(int number)
+    {
+        return Enumerable.Range(1, number)
+                .Where(x => number % x == 0)
+                .ToList();
+    }
+
+    public IEnumerable<int> GetPrimes(IEnumerable<int> numbers)
+    {
+        var teste = numbers.Where(x => IsPrime(x)).ToList();
+        return teste;
+    }
+
+    public bool IsPrime(int number)
+    {
+        if (number == 1)
+            return false;
+
+        int numberDivisors = Enumerable.Range(1, number)
+                                .Count(x => (number % x == 0) && (x != number) && (x != 1));
+
+        return numberDivisors == 0 ? true : false;
+    }
+
+    #endregion Methods
+
 }
 
