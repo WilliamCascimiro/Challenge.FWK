a8f41fea2c988f940411cf6ebbe86493f2e79afc
William Cascimiro da Silva Alves <william.alves@trescon.com.br>
2022-03-08 20:59:34 -0300

    Criação do serviço de API

---
+++ Challenge.API/Controllers/CalculationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Challenge.API.DTO;
+using Challenge.Service;
+
+namespace Challenge.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculationController : ControllerBase
+    {
+        private readonly CalculationService _calculationService;
+
+        public CalculationController(CalculationService calculationService)
+        {
+            _calculationService = calculationService;
+        }
+
+        [HttpGet(Name = "Calculate/{number}")]
+        public IActionResult Calculate(int number)
+        {
+            var calculation = _calculationService.Calculate(number);
+            return Ok(new CalculationDTO(number, calculation.Item1, calculation.Item2));
+        }
+
+    }
+}
---
+++ Challenge.API/DTO/CalculationDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Challenge.API.DTO
+{
+    public class CalculationDTO
+    {
+        public CalculationDTO(int informedNumbers, IEnumerable<int> divisors, IEnumerable<int> primes)
+        {
+            InformedNumbers = informedNumbers;
+            Divisors = divisors;
+            Primes = primes;
+        }
+
+        public int InformedNumbers { get; private set; }
+        public IEnumerable<int> Divisors { get; private set; }
+        public IEnumerable<int> Primes { get; private set; }
+    }
+}
