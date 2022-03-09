e0cab9ebd0e377d2c2dbefb0933dd81da4662a9f
William Cascimiro da Silva Alves <william.alves@trescon.com.br>
2022-03-08 21:03:11 -0300

    Remoção de comentários

--- Challenge.Service/CalculationService.cs
+++ Challenge.Service/CalculationService.cs
@@ -1,25 +1,6 @@
 ﻿namespace Challenge.Service;
 public class CalculationService
 {
-    //public void Start()
-    //{
-    //    int informedNumber;
-    //    while (true)
-    //    {
-    //        Console.WriteLine("Informe um número:");
-    //        if (int.TryParse(Console.ReadLine(), out informedNumber) && informedNumber > 0)
-    //            break;
-    //        Console.WriteLine("Por favor, informe um número inteiro positivo");
-    //    }
-
-    //    var divisors = GetDivisors(informedNumber);
-    //    var primes = GetPrimes(divisors);
-
-    //    Console.WriteLine(@"Número de Entrada: " + String.Join(", ", informedNumber));
-    //    Console.WriteLine(@"Número Divisores: " + String.Join(", ", divisors));
-    //    Console.WriteLine(@"Divisores Primos: " + String.Join(", ", primes));
-    //}
-
     public (IEnumerable<int>, IEnumerable<int>) Calculate(int number)
     {
         var divisors = GetDivisors(number);
