9d9690ed0962a2f3d4f8cba417a7ae5b73983646
William Cascimiro da Silva Alves <william.alves@trescon.com.br>
2022-03-08 21:01:08 -0300

    Reescrita do "core" da aplicação para desacoplar do projeto de console.

--- Challenge.App/Program.cs
+++ Challenge.App/Program.cs
@@ -1,4 +1,19 @@
-﻿using Service;
+﻿using Challenge.Service;
 
-var calculation = new CalculationService();
-calculation.Start();
+var calculationService = new CalculationService();
+
+int informedNumber;
+while (true)
+{
+    Console.WriteLine("Informe um número:");
+    if (int.TryParse(Console.ReadLine(), out informedNumber) && informedNumber > 0)
+        break;
+    Console.WriteLine("Por favor, informe um número inteiro positivo");
+}
+
+var divisors = calculationService.GetDivisors(informedNumber);
+var primes = calculationService.GetPrimes(divisors);
+
+Console.WriteLine(@"Número de Entrada: " + String.Join(", ", informedNumber));
+Console.WriteLine(@"Número Divisores: " + String.Join(", ", divisors));
+Console.WriteLine(@"Divisores Primos: " + String.Join(", ", primes));
